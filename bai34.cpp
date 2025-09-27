226c435890fed771d04d6b0cb4d9dfc99eccef84
Hong Ngoc <hongngocnguyenthi4150@gmail.com>
2025-09-27 22:00:28 +0700

    Bai34

---
+++ bai34.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout << "Nhap hai so nguyen a va b: ";
+    cin >> a >> b;
+    cout << "Tong cua a va b la:" << a + b<< endl;
+    return 0;
+}
