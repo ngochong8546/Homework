e789b7c7fba413e65281d413d6cbd82efb5038ab
Hong Ngoc <hongngocnguyenthi4150@gmail.com>
2025-09-27 22:54:43 +0700

    Bai38

---
+++ bai38.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int max ( int a, int b, int c) {
+    if (a>=b && a>=c) {
+        return a;
+    } else if (b>=a && b>=c) {
+        return b;
+    } else {
+        return c;
+    }
+    }
+int main() {
+    int a,b,c;
+     cout << "Nhap ba so nguyen a, b, c:  ";
+     cin >> a >> b >> c;
+     cout << "So lon nhat trong ba so la: " << max(a,b,c) << endl;
+     return 0;
+}
